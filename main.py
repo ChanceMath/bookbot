d80051e08bee5f8f78c13d6bcd38b59b76e651df
ChanceMath <Chance.g.mathisen@gmail.com>
2025-02-20 18:56:14 -0800

    initial function structure

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("hello world")
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(f"{num_words} words found in the document")
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+def get_book_text(book_path):
+    with open(book_path) as f:
+        return f.read()
+
+def count_characters(text):
+    chars = {}
+    for char in text:
+        char = char.lower()
+        if char in chars:
+            chars[char] += 1
+        else:
+            chars[char] = 1
+    
+    return chars
+
+main()
